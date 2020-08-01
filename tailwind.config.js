2ae4494b97955dd6fbe162b0492b06c162ac71d7
olaysco <olayiwolaodunsi@gmail.com>
2020-08-01 17:43:20 +0100

    update purgecss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     purge: [
+        './resources/js/components/atoms/*.vue',
+        './resources/js/components/icons/*.vue',
+        './resources/js/components/molecules/*.vue',
+        './resources/js/components/organisms/*.vue',
         './resources/js/components/*.vue',
-        './resources/js/pages/*.vue'
+        './resources/js/pages/*.vue',
+        './resources/js/pages/Auth/*.vue',
+        './resources/js/pages/Education/*.vue'
       ],
   theme: {
     extend: {},
